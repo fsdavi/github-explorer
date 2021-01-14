84786d4a72d9ea70689506e4e688ce560d8fc982
Davi <fsdavi.dev@gmail.com>
2021-01-13 21:07:10 -0300

    Connect to the github api, and start to list the repositories in the dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,74 @@
-import React from 'react';
-import { Title, Form, Repositories } from './styles';
+import React, { FormEvent, useState } from 'react';
+import { Title, Form, Repositories, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/Logo.svg';
 
+interface Repository {
+    full_name: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+
+    };
+    description: string;
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    const handleAddRepository = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório');
+        }
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
-            <Form>
-                <input type="text" placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}
+                hasError={!!inputError}
+            >
+                <input type="text" placeholder="Digite o nome do repositório"
+                    value={newRepo}
+                    onChange={(event) => {
+                        setNewRepo(event.target.value);
+                        console.log(newRepo);
+                    }}
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
+            {inputError !== '' && <Error>{inputError}</Error>}
             <Repositories>
-                <a href="test">
-                    <img src="https://avatars1.githubusercontent.com/u/57576938?s=460&u=821f209d59f35fc3621cac5178f530624e6a38ae&v=4" alt="Davi Freire" />
+                {repositories.map(repository =>
+                (<a key={repository.full_name} href="test">
+                    <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                     <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>ushdauhsu aushdeauhsduahs aushduasd</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
-
                     <FiChevronRight size={20} />
-                </a>
-                <a href="test">
-                    <img src="https://avatars1.githubusercontent.com/u/57576938?s=460&u=821f209d59f35fc3621cac5178f530624e6a38ae&v=4" alt="Davi Freire" />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>ushdauhsu aushdeauhsduahs aushduasd</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="test">
-                    <img src="https://avatars1.githubusercontent.com/u/57576938?s=460&u=821f209d59f35fc3621cac5178f530624e6a38ae&v=4" alt="Davi Freire" />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>ushdauhsu aushdeauhsduahs aushduasd</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="test">
-                    <img src="https://avatars1.githubusercontent.com/u/57576938?s=460&u=821f209d59f35fc3621cac5178f530624e6a38ae&v=4" alt="Davi Freire" />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>ushdauhsu aushdeauhsduahs aushduasd</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
+                </a>)
+                )}
             </Repositories>
         </>
     );
