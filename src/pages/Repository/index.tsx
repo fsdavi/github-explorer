b316c961006ca980c48c46287018718105375efb
Davi <fsdavi.dev@gmail.com>
2021-01-15 23:42:33 -0300

    Stylize the details page of the repositories.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { Header, RepositoryInfo, Issues } from './styles';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/Logo.svg'
 
 interface RepositoryParams {
     repository: string;
@@ -9,7 +13,63 @@ const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
     return (
-        <h1>Opa</h1>
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src={logoImg} alt="Owner" />
+                    <div>
+                        <strong>
+                            {params.repository}
+                        </strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>
+                            1808
+                        </strong>
+                        <span>
+                            Stars
+                        </span>
+                    </li>
+                    <li>
+                        <strong>
+                            48
+                        </strong>
+                        <span>
+                            Forks
+                        </span>
+                    </li>
+                    <li>
+                        <strong>
+                            67
+                        </strong>
+                        <span>
+                            Issues
+                        </span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+            <Link to={`/repositories/`}>
+                    <div>
+                        <strong>repository.full_name</strong>
+                        <p>repository.description</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
     );
 }
 
